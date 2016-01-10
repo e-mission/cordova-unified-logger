7ca1bce8af35627c3c7b191282e29c0401197b8c
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-10 11:50:33 -0800

    Fixed the code to return entries by index
    There is now a separate function to return the max index, and the main code
    returns all entries that are less than the index since we display values in
    reverse chron order.
    
    Also fixed the error handlers to be as per sqlite plugin expectations - namely
    that they take two arguments, the second of which is the error, and they return
    false.

--- www/unifiedlogger.js
+++ www/unifiedlogger.js
@@ -41,10 +41,38 @@ var ULogger = {
         createFromLocation: 1
     }),
 
+    /*
+     * The issue here is that we are returning the log in reverse chron order,
+     * so that users don't have to load the entire database in order to see
+     * what's going on right now.  This means that we need to start the current
+     * index at the max index.  Unfortunately, we don't know what that is until
+     * we have started reading the database. This function returns the max
+     * index, to be passed to the getMessagesFromIndex function.
+     */
+    getMaxIndex: function (successCallback, errorCallback) {
+        ULogger.db.transaction(function(tx) {
+            var maxIndexQuery = "SELECT MAX(ID) FROM "+ULogger.LOG_TABLE;
+            console.log("About to execute query "+maxIndexQuery+" against "+ULogger.LOG_TABLE);
+            tx.executeSql(maxIndexQuery,
+                [],
+                function(tx, data) {
+                    if (data.rows.length == 0) {
+                        errorCallback("no max index returned - are there any index values? unable to retrieve logs")
+                    } else {
+                        var maxIndex = data.rows.item(0)["MAX(ID)"];
+                        successCallback(maxIndex);
+                    }
+                }, function(e, response) {
+                    errorCallback(response);
+                    return false;
+                })
+        })
+    },
+
     getMessagesFromIndex: function (startIndex, count, successCallback, errorCallback) {
         ULogger.db.transaction(function(tx) {
             var selQuery = "SELECT * FROM "+ULogger.LOG_TABLE+
-                           " WHERE "+ULogger.KEY_ID+" > "+startIndex+
+                           " WHERE "+ULogger.KEY_ID+" < "+startIndex+
                            " ORDER BY "+ULogger.KEY_ID+" DESC LIMIT "+count;
             // Log statements in the logger don't go into the logger.
             // No infinite loop here!
@@ -61,15 +89,15 @@ var ULogger = {
                     }
                     successCallback(resultList);
                 },
-                function(e) {
-                    errorCallback(e);
+                function(e, response) {
+                    errorCallback(response);
+                    return false;
                 });
         });
     },
 
     getMessagesForRange: function (startTime, endTime, successCallback, errorCallback) {
     }
-
 }
 
 module.exports = ULogger;
